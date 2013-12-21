8005d53a6d9e549a5d3c388c21f42b35e182bf81
Eddie Dickey <eddie.dickey@gmail.com>
2013-12-20 19:35:36 -0800

    small changes

--- routes/applications.js
+++ routes/applications.js
@@ -14,7 +14,6 @@ exports.list = function(req, res, next){
 };
 
 exports.update = function(req, res, next){
-	console.log("req.body.Status ", req.body.Status);
   req.db.applications.updateById(req.application._id, {$set: {Status: req.body.Status === 'expired'}}, function (error, count) {
     if (error) return next(error);
     if (count !==1) return next(new Error('Something went wrong.'));
