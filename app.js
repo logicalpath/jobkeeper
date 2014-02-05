3938ef8ea88bf384b38b0df1790c939fd9538db5
Eddie Dickey <eddie.dickey@gmail.com>
2014-02-04 20:52:12 -0800

    change port to 3000

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
 })
 app.locals.appname = 'Job Keeper'
 
-app.set('port', process.env.PORT || 3003);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
